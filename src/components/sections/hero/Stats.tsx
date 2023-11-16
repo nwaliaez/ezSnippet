dcbf281e66730d1f1c965c4f980199f93f959b09
Hemanth Kumar M <083hemanth@gmail.com>
2023-11-16 16:24:35 +0530

    Update Stats.tsx
    I have updated your Instagram profile

--- src/components/sections/hero/Stats.tsx
+++ src/components/sections/hero/Stats.tsx
@@ -10,13 +10,13 @@ const Stats: FC<StatsProps> = ({}) => {
             <List link="/instagram" sizes="xs" className="gap-4">
                 <User size={16} />
                 <span>
-                    <b className="text-secondary">106k</b> Followers
+                    <b className="text-secondary">788k</b> Followers
                 </span>
             </List>
             <List link="/instagram" sizes="xs" className="gap-4">
                 <Video size={16} />
                 <span>
-                    <b className="text-secondary">102</b> Videos
+                    <b className="text-secondary">517</b> Videos
                 </span>
             </List>
             <List link="/instagram" sizes="xs" className="gap-4">
